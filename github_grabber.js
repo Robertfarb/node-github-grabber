ae67f589fe7612666a8c6573426dd37e7f40a7a0
Robert Farb <farb.robert@gmail.com>
2018-11-08 17:34:13 -0800

    Complete github grabber node.js project

--- github_grabber.js
+++ github_grabber.js
@@ -3,12 +3,12 @@ const http = require('http');
 const qs = require('querystring');
 const https = require('https');
 
-const createOptionsObj = (username) => {
+function createOptionsObj(username) {
   return {
     hostname: 'api.github.com',
     path: `users/${username}/starred`,
     headers: {
-      'User-Agent'
+      'User-Agent': 'github-grabber'
     }
   }
 }
@@ -16,25 +16,30 @@ const createOptionsObj = (username) => {
 const githubServer = http.createServer((req, res) => {
   if (req.method === 'POST') {
     let body = '';
-    req.on('data', d => {
+    req.on('data', (d) => {
       body += d;
     });
     req.on('end', () => {
       const username = qs.parse(body).username
       const ws = fs.createWriteStream(`/${username}_starred_repos.txt`);
       const opts = createOptionsObj(username);
+      console.log(opts);
       https.get(opts, (dataStream) => {
         let repoData = '';
         dataStream.on('data', d => { repoData += d });
         dataStream.on('end', () => {
-          const repos = JSON.parse(repoData).map(repo => {
-            return `Repo: ${repo.name}. Stars: ${repo.stargazers_count}.`;
-          }).join('\n')
-          ws.write(repos);
-          res.end(repos)
+          // const repos = JSON.parse(repoData).map(repo => {
+          //   return `Repo: ${repo.name}. Stars: ${repo.stargazers_count}.`;
+          // }).join('\n')
+          // ws.write(repos);
+          // console.log(repos);
+          // res.end(repos)
+          res.end(repoData);
+        }).on('error', (e) => {
+          console.error(e)
         })
       })
-    })
+     })
   }
 });
 
